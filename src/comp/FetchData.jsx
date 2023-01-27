ef11389b5c03c9a26fccffb144f0becf4f6c29e7
Taras <81424893+tar1kkk@users.noreply.github.com>
2023-01-27 23:51:43 +0200

    create func for Form component

--- src/comp/FetchData.jsx
+++ src/comp/FetchData.jsx
@@ -1,19 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchWord } from '../redux/slices/ItemSlice';
 import Form from './Form';
 
 function FetchData() {
-	const word = useSelector(state => state.ItemSlice.word);
-	const dispatch = useDispatch();
-
-	async function fetch() {
-		dispatch(fetchWord(word));
-	}
-	useEffect(() => {
-		fetch();
-		console.log(word)
-	}, [word]);
 	return (
 		<div>
 			<Form />
--- src/comp/Form.jsx
+++ src/comp/Form.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeWord } from '../redux/slices/ItemSlice';
+import { fetchWord } from '../redux/slices/ItemSlice';
 
 function Form() {
+	const item = useSelector(state => state.ItemSlice.item);
+	const word = useSelector(state => state.ItemSlice.word);
+	console.log(item)
+	async function fetch() {
+		dispatch(fetchWord(word));
+	}
 	const dispatch = useDispatch();
 	const valueWord = useSelector(state => state.ItemSlice.word);
+	const searching = (e) => {
+		e.preventDefault();
+		fetch();
+	}
 	return (
 		<div className="app">
 			<main className="main">
@@ -20,13 +31,15 @@ function Form() {
 							onChange={(e) => dispatch(changeWord((e.target.value)))}
 						/>
 					</div>
-					<button onClick={(e) => e.preventDefault()}>Search</button>
+					<button onClick={(e) => searching(e)}>Search</button>
 				</form>
 
 				<div className="error"></div>
 				<div className="results">
 					<div className="results-info">
-						<div className="results-word"></div>
+						<div className="results-word">
+
+						</div>
 
 						<div className="results-sound">
 							<img src="./assets/images/sound.png" alt="" />
